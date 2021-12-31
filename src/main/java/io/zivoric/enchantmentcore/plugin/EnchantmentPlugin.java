2ed7b401e9dc85c2334880c8ed58141c1942a71d
Huynh Tien <huynhqtienvtag@gmail.com>
2021-12-31 11:27:49 +0700

    comment for this

--- src/main/java/io/zivoric/enchantmentcore/plugin/EnchantmentPlugin.java
+++ src/main/java/io/zivoric/enchantmentcore/plugin/EnchantmentPlugin.java
@@ -4,6 +4,16 @@ import io.zivoric.enchantmentcore.CustomEnch;
 
 import java.util.List;
 
+/**
+ * This interface will be implemented by plugins to add new enchants.
+ * Developers need to implement this interface to their main plugin class.
+ * This core will detect all plugins implementing this interface and add their enchants to the core.
+ */
 public interface EnchantmentPlugin {
+    /**
+     * Get the list of enchantments from this plugin
+     *
+     * @return List of enchantments
+     */
     List<CustomEnch> getEnchants();
 }
